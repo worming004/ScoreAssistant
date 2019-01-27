b594c2fd6e81804aeaa091a476c90e856ffa3af7
Mathieu <worming@hotmail.be>
2019-01-27 18:01:46 +0100

    initial commit. Do not works

---
+++ OpenSessionBot/ScoreAssistantBot.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenSessionBot
+{
+    public class ScoreAssistantBot : IBot
+    {
+        private readonly OpenSessionBotAccessors _accessors;
+        private readonly ILogger _logger;
+
+        public ScoreAssistantBot(OpenSessionBotAccessors accessors, ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _logger = loggerFactory.CreateLogger<OpenSessionBotBot>();
+            _logger.LogTrace("Turn start.");
+            _accessors = accessors ?? throw new System.ArgumentNullException(nameof(accessors));
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Handle Message activity type, which is the main activity type for shown within a conversational interface
+            // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
+            // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                // Get the conversation state from the turn context.
+                var state = await _accessors.ScoreState.GetAsync(turnContext, () => new ScoreState());
+
+                // Bump the turn count for this conversation.
+                state.TurnCount++;
+
+                // Set the property using the accessor.
+                await _accessors.ScoreState.SetAsync(turnContext, state);
+
+                // Save the new turn count into the conversation state.
+                await _accessors.ConversationState.SaveChangesAsync(turnContext);
+
+                // Echo back to the user whatever they typed.
+                var responseMessage = $"Turn {state.TurnCount}: You sent '{turnContext.Activity.Text}'\n";
+                await turnContext.SendActivityAsync(responseMessage);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected");
+            }
+        }
+    }
+}
---
+++ OpenSessionBot/ScoreState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenSessionBot
+{
+    public class ScoreState
+    {
+        public IDictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+    }
+}
